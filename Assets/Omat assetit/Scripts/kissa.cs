6505e4fa2072086b99b0f5e393433c6a79f8587e
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2022-11-29 17:09:28 +0200

    liikkuminen

--- Assets/Omat assetit/Scripts/kissa.cs
+++ Assets/Omat assetit/Scripts/kissa.cs
@@ -6,6 +6,11 @@ public class kissa : MonoBehaviour
 {
 	public CharacterController controller;
 	
+	private float verticalSpeed = 0f;
+	private float horizontalSpeed = 0f;
+	
+	public int speed = 3;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,9 @@ public class kissa : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        verticalSpeed = Input.GetAxis("Vertical");
+		horizontalSpeed = Input.GetAxis("Horizontal");
+		Vector3 direction = new Vector3(horizontalSpeed, -1, verticalSpeed);
+		controller.Move(direction * Time.deltaTime * speed);
     }
 }
